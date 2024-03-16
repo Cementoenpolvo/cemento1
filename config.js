f44025d0d6ffee71af981724bfcfe3dbf25a9c85
SKILLEX MONSTER <127691339+Cementoenpolvo@users.noreply.github.com>
2024-03-16 16:46:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,12 +13,12 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["573026191480", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["593968585383"], 
-["5492266613038"], 
-["593968263524"],
-["33760536110"],
-["573147616444"]]
+["51977338006", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["51941842354"], 
+["51900422065"], 
+[""],
+[""],
+[""]]
 
 global.mods = []
 global.prems = []
